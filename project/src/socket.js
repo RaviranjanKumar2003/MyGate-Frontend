bb463e5cfc9a263f140059bdae76d2ae8adaf0be
Raviranjan Kumar <raviranjankumarnd2003@gmail.com>
2026-04-01 19:35:44 +0530

    fix axios4

--- project/src/socket.js
+++ project/src/socket.js
@@ -1,7 +1,7 @@
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
 
-const socket = new SockJS("http://localhost:9090/call-socket");
+const socket = new SockJS("https://mygate-backend-tmre.onrender.com/call-socket");
 const stompClient = Stomp.over(socket);
 
 export default stompClient;
