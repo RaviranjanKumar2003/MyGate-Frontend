554c350f90084abdef9adafce7e1f6e0e6e3eb94
Raviranjan Kumar <raviranjankumarnd2003@gmail.com>
2026-04-01 19:41:42 +0530

    fix axios4

--- project/src/api/axios.js
+++ project/src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // ✅ LOCAL (development)
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
